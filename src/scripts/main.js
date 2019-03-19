284fa28b0d6ac294b8334a4b22f7e1074a6ee2a8
Sam Alpren <samalpren1@gmail.com>
2019-03-19 15:43:43 -0500

    learned filter method and map method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,76 @@
 const outEl = document.querySelector("#output")
 outEl.innerHTML = "<h1>Active Businesses</h1>"
 
-let zipCodeProperty = "addressZipCode"
+// let zipCodeProperty = "addressZipCode"
 
-businesses.forEach(business => {
-  outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
-    <section>
-      ${business.addressFullStreet}
-    </section>
-    <section>
-      ${business.addressCity}, ${business['addressStateCode']} ${business[zipCodeProperty]} 
-    </section>
-  `
-  outEl.innerHTML += "<hr/>"
+// businesses.forEach(business => {
+//   outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     // <section>
+//     //   ${business.addressCity}, ${business['addressStateCode']} ${business[zipCodeProperty]} 
+//     // </section>
+//   `
+//   outEl.innerHTML += "<hr/>"
+// });
+
+
+// Array to contain all the New York businesses
+// const newYorkBusinesses = businesses.filter(business => {
+//   let inNewYork = false
+
+//   if (business.addressStateCode === "NY") {
+//       inNewYork = true
+//   }
+
+//   return inNewYork
+// })
+
+// console.log(newYorkBusinesses);
+
+// newYorkBusinesses.forEach(business => {
+//   outEl.innerHTML += `
+//   <h2>${business.companyName}</h2>
+//   <section>
+//     ${business.addressFullStreet}
+//   </section>
+//   // <section>
+//   //   ${business.addressCity}, ${business['addressStateCode']} ${business[zipCodeProperty]} 
+//   // </section>
+// `
+// outEl.innerHTML += "<hr/>"
+// });
+
+// const manufacturingBusinesses = businesses.filter(business => {
+//   let ifManufacturer = false
+//   if (business.companyIndustry === "Manufacturing") {
+//     return true
+//   } else {
+//     return false
+//   }
+// })
+
+outEl.innerHTML += "<h1>Purchasing Agents</h1>";
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+const agents = businesses.map(business => {
+    return {
+      fullName: `${business.purchasingAgent.nameFirst} ${business.purchasingAgent.nameLast}`,
+      company: business.companyName,
+      phoneNumber: business.phoneWork
+    }
+})
+
+console.table(agents)
+
+agents.forEach(agent => {
+  outEl.innerHTML += `<h2>${agent.fullName}</h2>`;
+  outEl.innerHTML += `<p>${agent.company}</p>`;
+  outEl.innerHTML += `<p>${agent.phoneNumber}</p>`;
+  outEl.innerHTML += "<hr/>";
 });
